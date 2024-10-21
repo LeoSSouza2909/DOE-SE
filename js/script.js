d28dea47d3817a298bab2a8e3cb04b7aceb9f68d
leonardosouza <lss.111819@gmail.com>
2024-10-21 20:03:52 -0300

    Modal de login criada

--- js/script.js
+++ js/script.js
@@ -56,3 +56,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     autoplay();
 });
+
+
+var modal = document.getElementById("login-modal");
+var btn = document.getElementById("btn-login");
+var span = document.getElementsByClassName("close-modal")[0];
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
